16d1ff54ca32d6199f4650a3dce5613454e5b1da
imnisi <nishantkrbihta01@gmail.com>
2025-11-16 22:00:54 +0530

    JavaScript basics lesson - 1 completed

---
+++ .vscode/Exercises/JavaScript Files/script.js
@@ -0,0 +1,20 @@
+// 1. Use alert(...); to display 'Good Morning!' in a popup.
+alert("Good Morning!");
+
+// 2. Display your name in a popup.
+alert("Hii! I am Nishant Kumar");
+
+// 3. Use document.body.innerHTML = ...; to display 'Good Morning!' on the web page.
+document.body.innerHTML = "Good Morning!";
+
+// 4. Display your name on the web page
+document.body.innerHTML = "Nishant Kumar";
+
+// 5. Your order a T-shirt for $10, socks for $8, and dinner plates for $20. Use JavaScript to calculate the total cost of your order.
+console.log("Total Cost: $", 10 + 8 + 20);
+
+// 6. Your bank account has $100, you spend $20 on lunch, $50 on dinner, amd earn $200 from your job. Calculate how much money you have now.
+console.log(" money remaining: $", 200 - 100 + 20 + 50);
+
+// 7. Use document.body.innerHTML = ...; to make the web page blank.
+document.body.innerHTML = "";
